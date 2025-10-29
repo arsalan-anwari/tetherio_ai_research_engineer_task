d729ee0205fbb42069ef25eddb1315d9453b2a0a
arsalan-anwari <arsalan.anwari@outlook.com>
2025-10-29 18:09:11 +0100

    First iteration to test if vulkan works with shaderc for runtime compiled shaders and testing llama.cpp library with more complex model. Cleaning the cmake a bit to support different architectures. This is very early alpha, just checking if everything works on my new laptop.

--- cmake/LinkTarget.cmake
+++ cmake/LinkTarget.cmake
@@ -3,5 +3,9 @@ if(TARGET_WIN32)
         PRIVATE
             llama
             Vulkan::Vulkan
+            unofficial::shaderc::shaderc
     )
+
+    target_compile_definitions(app PRIVATE USE_SHADERC=1)
+
 endif()
